1ff293f16d50881f3cdadb55a3dee8bcdd7834b2
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-01-12 10:21:40 -0600

    Initial Commit

---
+++ tic tac toe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tic tac toe
+//
+//  Created by Student on 1/12/21.
+//  Copyright © 2021 elenator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
